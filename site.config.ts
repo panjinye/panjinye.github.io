4b8d45916e2ff570473b6731c5a0339b9f1c177c
panjinye <panjinye@gmail.com>
2026-01-23 16:22:27 +0800

    Lawpan First

--- site.config.ts
+++ site.config.ts
@@ -1,8 +1,8 @@
 import siteConfig from "./src/utils/config";
 
 const config = siteConfig({
-	title: "Ficor",
-	prologue: "一个重新站起来，准备出发的人\nDo one Thing at a Time, and Do Well.",
+	title: "Ficor's Path of the Law",
+	prologue: "在路上的思绪与脚印\nDo one Thing at a Time, and Do Well.",
 	author: {
 		name: "Ficor",
 		email: "ficor@qq.com",
